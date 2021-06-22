7281f872eccfd25e362bdf1230b59c15ec340a67
Didem Tumer <didemdizman@gmail.com>
2021-06-22 13:59:52 +0300

    Refactor Code

--- script.js
+++ script.js
@@ -18,37 +18,45 @@ document.querySelector('.again').addEventListener('click',
 		document.querySelector('.number').style.width = '15rem';
 	})
 
-document.querySelector('.check').addEventListener('click',
-	function () {
-		const guess = Number(document.querySelector('.guess').value);
-		console.log(typeof guess, guess);
-		if (!guess) {
-			document.querySelector('.message').textContent = 'No Number';
-		} else if (guess === secretNumber) {
-			document.querySelector('.message').textContent = 'Correct Number';
-			document.querySelector('.number').textContent = secretNumber;
-			document.querySelector('body').style.backgroundColor = '#60b347';
-			document.querySelector('.number').style.width = '30rem';
-			if (score > highscore) {
-				highscore = score;
-				document.querySelector('.highscore').textContent = highscore;
-			}
-		} else if (guess > secretNumber) {
-			if (score > 1) {
-				document.querySelector('.message').textContent = 'Too High'
-				score--;
-				document.querySelector('.score').textContent = score;
-			} else {
-				document.querySelector('.message').textContent = 'You lost the game'
-			}
-		} else if (guess < secretNumber) {
-			if (score > 1) {
-				document.querySelector('.message').textContent = 'Too Low'
-				score--;
-				document.querySelector('.score').textContent = score;
-			} else {
-				document.querySelector('.message').textContent = 'You lost the game'
-			}
+document.querySelector('.check').addEventListener('click', function () {
+	const guess = Number(document.querySelector('.guess').value);
+	console.log(typeof guess, guess);
+	if (!guess) {
+		document.querySelector('.message').textContent = 'No Number';
+	} else if (guess === secretNumber) {
+		document.querySelector('.message').textContent = 'Correct Number';
+		document.querySelector('.number').textContent = secretNumber;
+		document.querySelector('body').style.backgroundColor = '#60b347';
+		document.querySelector('.number').style.width = '30rem';
+		if (score > highscore) {
+			highscore = score;
+			document.querySelector('.highscore').textContent = highscore;
 		}
-	})
+	} else if (guess !== secretNumber) {
+		if (score > 1) {
+			document.querySelector('.message').textContent = guess > secretNumber ? 'Too High!!' : 'Too Low!!'
+			score--;
+			document.querySelector('.score').textContent = score;
+		} else {
+			document.querySelector('.message').textContent = 'You lost the game'
+		}
+	}
+	/* else if (guess > secretNumber) {
+		if (score > 1) {
+			document.querySelector('.message').textContent = 'Too High'
+			score--;
+			document.querySelector('.score').textContent = score;
+		} else {
+			document.querySelector('.message').textContent = 'You lost the game'
+		}
+	} else if (guess < secretNumber) {
+		if (score > 1) {
+			document.querySelector('.message').textContent = 'Too Low'
+			score--;
+			document.querySelector('.score').textContent = score;
+		} else {
+			document.querySelector('.message').textContent = 'You lost the game'
+		}
+	} */
+})
 
